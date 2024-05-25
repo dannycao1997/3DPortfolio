016f340b29c3d7206c8a4fc62aa182c70c53bed0
Danny Cao <dannycao1997@yahoo.com>
2024-05-25 09:13:59 -0400

    Normalize all the line endings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,7 @@ const Navbar = () => {
                     <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
                     <p className='text-white text-[18px] font-bold cursor-pointer flex '>
                         Danny Cao&nbsp;
-                        <span className='sm:block hidden'> | JavaScript Mastery</span>
+                        <span className='sm:block hidden'> | Zip Coder</span>
                     </p>
                 </Link>
 
