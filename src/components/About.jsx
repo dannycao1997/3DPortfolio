aaaa2c027044e858d3a72f311017466c5ec036fb
Danny Cao <dannycao1997@yahoo.com>
2024-07-07 15:44:03 -0400

    Make changes and update about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,17 +45,18 @@ const About = () => {
                  className="text-blue-400 hover:text-blue-800">
                   https://www.linkedin.com/in/dannycao97/
               </a>
+              <br />
+              <a href="https://github.com/dannycao1997" target="_blank" rel="noopener noreferrer"
+                 className="text-blue-400 hover:text-blue-800">
+                  https://github.com/dannycao1997
+              </a>
           </motion.div>
 
           <motion.p
               variants={fadeIn("", "", 0.1, 1)}
               className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
           >
-              Admitted into Zip Code Wilmington’s highly competitive software development
-            program accepting less than 10% of applicants; completed over 1000 hours of software
-            development projects in an immersive, full-stack training program using Agile and
-            Scrum methodologies and test-driven development; gained experience with multiple
-            object-oriented programming languages and frameworks.
+              Transitioning seamlessly from nearly ten years in automotive service diagnostics, I bring a unique blend of analytical prowess and client-focused dedication to the software development field. My deep technical expertise and leadership experience enable me to tackle complex software challenges with precision. Proficient in Java, Spring Boot, and modern front-end technologies, I specialize in creating robust, scalable applications that drive results and exceed client expectations. My proven ability to translate diagnostic skills into software solutions makes me an asset in any development team.
         </motion.p>
 
         <div className='mt-20 flex flex-wrap gap-10'>
